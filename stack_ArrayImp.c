9023d90009e90dbc8e7d598c8c874213a259426d
Re-Yan <reannub@gmail.com>
2024-05-20 19:39:55 +0800

    Rename file
    - added printf statement to ask for user input for stack size

---
+++ stack_ArrayImp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int stkSize;
+
+    printf("Enter the size of the Stack: ");
+    scanf("%d", &stkSize);
+
+    printf("The size of the stack is: %d", stkSize);
+
+    // int *queue = malloc(stkSize * sizeof(int));
+
+
+    return 0;
+}
